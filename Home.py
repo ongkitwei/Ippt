3ed0fd5bcaccba34280b72d98160cc774ddd28d7
KIT WEI <ongkitwei@gmail.com>
2023-09-17 22:49:10 +0800

    add files and code

---
+++ Home.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import data
+
+st.set_page_config(layout="centered",
+                   page_title="IPPT calculator")
+st.title("Ippt Calculator")
+option = st.selectbox("Choose Age:",
+                        (data.get_age())
+)
+print(option)
+print(type(option))
+
+col1,colmid,col2= st.columns([3,5,3])
+with col1:
+    st.image("Images/pushup.png", width=230)
+    pushup_reps = st.number_input(label="Pushup reps", step=1, min_value=0, key="pushup_reps_key")
+    print(pushup_reps)
+
+with col2:
+    st.image("Images/situp.png", width=230)
+    situp_reps = st.number_input(label="Situp reps", step=1, min_value=0, key="situp_reps_key")
+
+
+
+col3,col4,col5 = st.columns(3)
+with col4:
+    st.image("Images/running.png", width=230)
+
+col6L, col6, col7, col7R = st.columns([0.65,1,1,0.65])
+minutes = col6.number_input("Minutes", min_value=0, max_value=59, step=1, key="run_time_minutes_key")
+seconds = col7.number_input("Seconds", min_value=0, max_value=59, step=1, key="run_time_seconds_key")
+
+st.write("")
+st.write("")
+colb1, colb2,colb3 = st.columns([1,1.5,1])
+calculate_button = colb2.button(label="CALCULATE",use_container_width=True)
+
+if pushup_reps:
+    print(type(pushup_reps))
+    with col1:
+        st.write(f'<b>{str(data.get_pushup_points(pushup_reps, age=option.strip()))} points</b>', unsafe_allow_html=True)
+
+if situp_reps:
+    with col2:
+        st.write(f'<b>{str(data.get_situp_points(situp_reps, option))} points</b>', unsafe_allow_html=True)
+
+if minutes or seconds:
+    print(type(minutes))
+    print(type(seconds))
+    with col6:
+        st.write(f'<b>{str(data.get_running_points(minutes, seconds, option))} points</b>', unsafe_allow_html=True)
+        st.write(minutes)
+        st.write(seconds)
+
+if calculate_button:
+    pushup_points = data.get_pushup_points(pushup_reps, age=option.strip())
+    situp_points = data.get_situp_points(situp_reps, option)
+    run_points = data.get_running_points(minutes, seconds, option)
+    total_points = pushup_points + situp_points + run_points
+    # total_points = float(pushup_points) + float(situp_points) + float(run_points)
+    # st.info(f'You did {pushup_reps} and it gave you {pushup_points} points')
+    # st.info(f'You did {situp_reps} and it gave you {situp_points} points')
+    print(type(pushup_points))
+    print(type(situp_points))
+    print(type(run_points))
+    st.info(f"Total: {total_points}")
+    if total_points >= 51:
+        st.info("Result: Pass")
+    else:
+        st.info("Result: Fail")
---
+++ data.py
@@ -0,0 +1,126 @@
+import pandas
+
+# Calculate pushup points
+# def get_pushup_points(pushup_reps):
+#     data = pandas.read_csv("pushup.csv",sep=",")
+#     for index, row in data[2:].iterrows():
+#         if row["Age"] == str(pushup_reps):
+#             # print("ok")
+#             print(f'{index}- {row["Age"]}')
+#             number = index
+#             for index, row in data[number:number+1].iterrows():
+#                 print(f'{index}- {row["<22"]}')
+#                 return row["<22"]
+#         else:
+#             return 25
+        
+# Calculate situps points
+def get_situp_points(situp_reps, age):
+    df = pandas.read_csv("situp.csv", sep=",")
+
+    if situp_reps == 0:
+        return 0
+    elif situp_reps in df.loc[:, "Age"]:
+        columns = df.columns
+        # list_column = [c for c in columns[1:]]
+        list_points = df.loc[df['Age'] == str(situp_reps), age].squeeze()
+        return list_points
+    else:
+        return df.loc[df["Age"] == "60", age].squeeze()
+        
+# Calculate 2.4Km points
+def get_running_points_old(minutes, seconds):
+    data = pandas.read_csv("run.csv",sep=",")
+    col = data.columns
+
+    # col[0] is "Age" column, col[1] is "<22" column
+    time_dict = {}
+    user_timing = int(str(minutes)+str(seconds))
+    print(user_timing)
+    for index, row in data[2:].iterrows():
+        time_dict[int(row[col[0]].replace(":",""))] = index
+
+    if user_timing in time_dict:
+        for index, row in data[2:].iterrows():
+            if index == time_dict[user_timing]:
+                return row[col[1]]
+    else:
+        print("timing not found")
+        for key in time_dict.keys():
+            if key>user_timing:
+                for index, row in data[2:].iterrows():
+                    if index == time_dict[key]:
+                        return row[col[1]]
+                break
+
+def get_pushup_points(pushup_reps, age):
+    df = pandas.read_csv("pushup.csv", sep=",")
+    # age = "<22"
+    if pushup_reps == 0:
+        return 0
+    elif pushup_reps in df.loc[:, "Age"]:
+        columns = df.columns
+        # list_column = [c for c in columns[1:]]
+        list_points = df.loc[df['Age'] == str(pushup_reps), age].squeeze()
+        return list_points
+    else:
+        return df.loc[df["Age"] == "60", age].squeeze()
+
+def get_running_points(minutes, seconds, age): 
+    df = pandas.read_csv("run.csv", sep=",")
+    list_time = df.loc[2:, "Age "]
+    list_time = [f'{l.replace(":",".")}' for l in list_time]
+
+    seconds_2d = f'{seconds:02d}'
+    print(list_time)
+    print("")
+    if float(f'{str(minutes)}.{str(seconds_2d)}') > float(df.loc[df["Age "] == "18:20", "Age "].squeeze().replace(":",".")):
+        return 0
+    else:
+        for index, li in enumerate(list_time, start=2):
+            if float(li) >= float(f'{str(minutes)}.{str(seconds_2d)}'):
+                print(f'end at {index}')
+                print(f'choose from {index+1}')
+                points = df.loc[index, age]
+                return points
+            else:
+                pass
+
+def get_age():
+    df = pandas.read_csv("situp.csv", sep=",")
+    columns = df.columns
+    list_column = [c for c in columns[1:]]
+    return list_column
+
+def replace():
+    anjing = "9:30"
+    print(anjing.replace(":",""))
+
+def test_function(anjing):
+    print("hello" + anjing)
+
+def conv_float():
+    min = 5
+    min_f = float(min)
+    print(float("9.4") > 9.3)
+    print(min_f)
+
+def fill_with_0():
+    number = 1
+    formatted_number = f'{number:02d}'
+    print(formatted_number)
+
+
+if __name__ == "__main__":
+    # get_situp_points(input("Enter situp reps: "))
+    # print(get_running_points())
+    # replace()
+    # anjing = " anjing"
+    # test_function(anjing=anjing.strip())
+    # print(get_pushup_points(pushup_reps="45"))
+    # conv_float()
+    # fill_with_0()
+    # get_running_points()
+    # conv_float()
+    df = pandas.read_csv("run.csv", sep=",")
+    print(df.loc[df["Age "] == "18:20", "Age "].squeeze())
